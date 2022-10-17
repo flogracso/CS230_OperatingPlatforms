c16abac7c82efe06d0cf3febe64d9619446dafcb
flogracso <93359594+flogracso@users.noreply.github.com>
2022-10-16 21:23:11 -0400

    Add files via upload

---
+++ gameauth/src/main/java/com/gamingroom/gameauth/auth/GameAuthorizer.java
@@ -0,0 +1,15 @@
+package com.gamingroom.gameauth.auth;
+
+import io.dropwizard.auth.Authorizer;
+
+public class GameAuthorizer implements Authorizer<GameUser> 
+{
+    @Override
+    public boolean authorize(GameUser user, String role) {
+    	
+        // FIXME: Finish the authorize method based on BasicAuth Security Example
+    	// 20220920 - retrieve role of the user
+    	return user.getRoles() != null && user.getRoles().contains(role);
+    	
+    }
+}
